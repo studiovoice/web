b51a838b576b54b152e32e91473a6e5deee0bbed
Thien An Tran <fxthienan@gmail.com>
2025-12-31 12:48:22 -0800

    feat: add banner warning users that automatic anonymization is not yet implemented

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next/types";
 import MapMediaLayout from "@/components/MapMediaLayout";
+import IconInformationCircle from "@/icons/IconInformationCircle";
 
 export const metadata: Metadata = {
   title: "Media Map",
@@ -10,5 +11,15 @@ export const metadata: Metadata = {
 };
 
 export default function Map() {
-  return <MapMediaLayout />;
+  return (
+    <>
+      <div className="w-fit md:mx-auto text-center text-sm text-gray-500 py-1 px-4 italic border shadow-xs rounded-sm mb-2 mx-4">
+        <IconInformationCircle size={17} className="inline-block mr-1" />
+        Automatic anonymization not yet implemented. Anything you upload will be
+        publicly visible, so please remove any identifying information you do
+        not want published.
+      </div>
+      <MapMediaLayout />
+    </>
+  );
 }
